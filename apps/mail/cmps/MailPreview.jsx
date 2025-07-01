6365e3d4bcd89b265f2bb750cb0423c966306c6b
omri-gendler <foromri.g@gmail.com>
2025-07-01 16:11:46 +0300

    update mail preview css

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -4,7 +4,7 @@ export function MailPreview({ mail }) {
     const readClass = mail.isRead ? 'read' : 'unread'
 
     return (
-        <div className={`mail-container ${readClass}`}>
+        <div className={`mail ${readClass}`}>
 
             <span className="mail">{!mail.isRead && <span style={{ color: '#0b57d0', marginLeft: '8px', fontSize: '10px' }}>●</span>}
                 {mail.from} </span>
