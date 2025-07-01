e537b822e7d6610de3c708f3ef6309bb0430170e
liat-kauffman <liatk770@gmail.com>
2025-07-01 17:04:30 +0300

    rough note preview and note list

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,18 @@
-export function NoteList() {
-    return <div>note list</div>
+export function NoteList(notes) {
+
+    if(!notes || notes.length === 0){
+        return <div className="no-notes">make a note</div>
+    }
+    return (<div>
+                <ul className="note-list">
+                    {notes.map(note =>(
+                        <NotePreview
+                            key={note.id}
+                            note={note}
+                        />
+                    ))}
+                    console.log(notes)
+                </ul>
+            </div>)
 }
+
