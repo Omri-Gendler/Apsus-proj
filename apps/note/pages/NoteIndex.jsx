adb4a90c953f2658130d75d6ab3a410d28a9b8f8
liat kauffman <liatK770@gmail.com>
2025-07-01 21:14:37 +0300

    update note db

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,6 +1,38 @@
-export function NoteIndex() {
-    return <section className="container">
-        {/* <NoteList/> */}
-        <h1>hi</h1>
-        </section>
+import { MailList } from "../cmps/MailList.jsx"
+import { MailFilter } from "../cmps/MailFilter.jsx"
+import { noteService } from "../services/note.service.js"
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+const { Link } = ReactRouterDOM
+const { useState, useEffect } = React
+export function NoteIndex({ logo }) {
+    const [notes, setNotes] = useState(null)
+    const [filterBy, setFilterBy] = useState(noteService.getDefaultFilter(''))
+    useEffect(() => {
+        loadNotes()
+    }, [])
+    function loadNotes() {
+        noteService.query()
+            .then(setNotes)
+            .catch(err => {
+                console.log('err:', err)
+            })
+    }
+    function onRemoveMails(noteId) {
+        noteService.remove(noteId)
+            .then(() => {
+                showSuccessMsg('Mail Removed Successfully!')
+                setNotes((prevMails) =>
+                    prevMails.filter(mail => mail.id !== noteId)
+                )
+            })
+            .catch(err => {
+                console.log(err)
+                showErrorMsg('Problem removing Mail')
+            })
+    }
+    // function onSetFilterBy(filterByToEdit) {
+    //     setFilterBy(prevFilter => ({ ...prevFilter, ...filterByToEdit }))
+    // }
+    if (!notes) return <div className="container">Loading...</div>
+    return console.log(notes)
 }
--- apps/note/services/util.service.js
+++ apps/note/services/util.service.js
@@ -1,27 +1,7 @@
-export const utilService = {
-    makeId,
-    makeLorem,
-    getRandomIntInclusive,
-    getRandomColor,
-    padNum,
-    getDayName,
-    getMonthName,
-    loadFromStorage,
-    saveToStorage
-}
 
-function saveToStorage(key, val) {
-    localStorage.setItem(key, JSON.stringify(val))
-}
-
-function loadFromStorage(key) {
-    const val = localStorage.getItem(key)
-    return JSON.parse(val)
-}
-
-function makeId(length = 6) {
+export function makeId(length = 6) {
     var txt = ''
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
     for (var i = 0; i < length; i++) {
         txt += possible.charAt(Math.floor(Math.random() * possible.length))
@@ -30,8 +10,8 @@ function makeId(length = 6) {
     return txt
 }
 
-function makeLorem(size = 100) {
-    const words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
+export function makeLorem(size = 100) {
+    var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
     var txt = ''
     while (size > 0) {
         size--
@@ -40,34 +20,28 @@ function makeLorem(size = 100) {
     return txt
 }
 
-function getRandomIntInclusive(min, max) {
+export function getRandomIntInclusive(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
 
-function padNum(num) {
-    return (num > 9) ? num + '' : '0' + num
+export function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
 }
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF'
-    var color = '#'
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)]
+export function loadFromStorage(key) {
+    const data = localStorage.getItem(key)
+    return (data) ? JSON.parse(data) : undefined
+}
+
+
+export function debounce(func, delay) {
+    let timeoutId
+    return (...args) => {
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(() => {
+            func(...args)
+        }, delay)
     }
-    return color
-}
-
-function getDayName(date, locale) {
-    date = new Date(date)
-    return date.toLocaleDateString(locale, { weekday: 'long' })
-}
-
-
-function getMonthName(date) {
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ]
-    return monthNames[date.getMonth()]
 }
